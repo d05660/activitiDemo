de954b2b2c4b36a1c58de789a1a48e4ef536aa3d
d05660 <d05660@sinocom.com>
2018-12-29 18:28:50 +0900

    add redis.
    Signed-off-by: d05660 <d05660@sinocom.com>

--- src/main/java/org/cloud/activiti/security/KickoutSessionControlFilter.java
+++ src/main/java/org/cloud/activiti/security/KickoutSessionControlFilter.java
@@ -2,7 +2,6 @@ package org.cloud.activiti.security;
 
 import java.io.Serializable;
 import java.util.Deque;
-import java.util.LinkedList;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -15,12 +14,18 @@ import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.AccessControlFilter;
 import org.apache.shiro.web.util.WebUtils;
+import org.cloud.activiti.entity.User;
 
+/**
+ * 
+ * @author sinocom
+ * @description: shiro 自定义filter 实现 并发登录控制
+ */
 public class KickoutSessionControlFilter extends AccessControlFilter {
 
-    private String kickoutUrl; // 踢出后到的地址
-    private boolean kickoutAfter = false; // 踢出之前登录的/之后登录的用户 默认踢出之前登录的用户
-    private int maxSession = 1; // 同一个帐号最大会话数 默认1
+    private String kickoutUrl; //踢出后到的地址
+    private boolean kickoutAfter = false; //踢出之前登录的/之后登录的用户 默认踢出之前登录的用户
+    private int maxSession = 1; //同一个帐号最大会话数 默认1
 
     private SessionManager sessionManager;
     private Cache<String, Deque<Serializable>> cache;
@@ -40,40 +45,40 @@ public class KickoutSessionControlFilter extends AccessControlFilter {
     public void setSessionManager(SessionManager sessionManager) {
         this.sessionManager = sessionManager;
     }
-
+    //设置Cache的key的前缀
     public void setCacheManager(CacheManager cacheManager) {
-        this.cache = cacheManager.getCache("shiro-activeSessionCache");
+        this.cache = cacheManager.getCache("shiro_redis_cache");
     }
 
     @Override
-    protected boolean isAccessAllowed(ServletRequest arg0, ServletResponse arg1, Object arg2) throws Exception {
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
         return false;
     }
 
     @Override
     protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
         Subject subject = getSubject(request, response);
-        if(!subject.isAuthenticated() && !subject.isRemembered()) {//未授权且非remember 
+        if(!subject.isAuthenticated() && !subject.isRemembered()) {
             //如果没有登录，直接进行之后的流程
             return true;
         }
- 
+
         Session session = subject.getSession();
-        String username = (String) subject.getPrincipal();
+        User user = (User) subject.getPrincipal();
+        String username = user.getUsername();
         Serializable sessionId = session.getId();
- 
-        //TODO 同步控制
+
+        //读取缓存   没有就存入
         Deque<Serializable> deque = cache.get(username);
-        if(deque == null) {
-            deque = new LinkedList<Serializable>();
-            cache.put(username, deque);
-        }
- 
+        
         //如果队列里没有此sessionId，且用户没有被踢出；放入队列
         if(!deque.contains(sessionId) && session.getAttribute("kickout") == null) {
+            //将sessionId存入队列
             deque.push(sessionId);
+            //将用户的sessionId队列缓存
+            cache.put(username, deque);
         }
- 
+
         //如果队列里的sessionId数超出最大会话数，开始踢人
         while(deque.size() > maxSession) {
             Serializable kickoutSessionId = null;
@@ -82,29 +87,34 @@ public class KickoutSessionControlFilter extends AccessControlFilter {
             } else { //否则踢出前者
                 kickoutSessionId = deque.removeLast();
             }
+            //踢出后再更新下缓存队列
+            cache.put(username, deque);
+            
+            
             try {
+                //获取被踢出的sessionId的session对象
                 Session kickoutSession = sessionManager.getSession(new DefaultSessionKey(kickoutSessionId));
                 if(kickoutSession != null) {
                     //设置会话的kickout属性表示踢出了
-                    //kickoutSession.setAttribute("kickout", true);
-                    kickoutSession.stop();
+                    kickoutSession.setAttribute("kickout", true);
                 }
             } catch (Exception e) {//ignore exception
             }
         }
- 
+
         //如果被踢出了，直接退出，重定向到踢出后的地址
-        if (session.getAttribute("kickout") != null) {
+        if ((Boolean)session.getAttribute("kickout")!=null&&(Boolean)session.getAttribute("kickout") == true) {
             //会话被踢出了
             try {
-                subject.logout();//这里走的shiro的退出
+                //退出登录
+                subject.logout();
             } catch (Exception e) { //ignore
             }
             saveRequest(request);
+            //重定向
             WebUtils.issueRedirect(request, response, kickoutUrl);
             return false;
         }
         return true;
     }
-
 }
